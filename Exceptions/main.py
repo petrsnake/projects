5cfc8465fa2b8981c582c7c6dafdbf6046c101ac
petrsnake <petrsnake@gmail.com>
2022-09-27 18:15:31 +0200

    exceptions 1

--- Exceptions/main.py
+++ Exceptions/main.py
@@ -1,3 +1,10 @@
 test = [1, 2, 3, 4]
-
-print (test[2])
+while True:
+    try:
+        a = int(input("zadej číslo 0 - 3: "))
+        print (test[a])
+    except IndexError as exc:
+        print("Zadávejte čísla pouze 0 - 3")
+    finally:
+        if a == 1:
+            break
