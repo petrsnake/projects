c1f2320dcea6cc59c8380979cc61bcf88c858088
Jan Harenčák <jan.harencak@firma.seznam.cz>
2022-09-27 21:36:46 +0200

    Added an example clas

--- Classes_and_methods/theory.py
+++ Classes_and_methods/theory.py
@@ -1,4 +1,6 @@
 from curses import init_color
+from re import S
+from unittest import makeSuite
 
 
 # Konstruktor, alokátor, initor
@@ -24,7 +26,42 @@ from curses import init_color
 class ExerciseClass:
     """tato třída je první pokus o pochopení tříd a metod
     """
-    def __init__(self) -> None:         #initor třídy 
+    def __init__(self) -> None: 
+        #initor třídy 
+        """
+        tato funkce se spusti pokazde kdyz tridu nejak inicializues..
+        proto se nazyva init..
+        napr:
+        myclass = ExerciseClass() by spustilo tuto funkci.. vyuziti nize
+        """
         pass
     
+
+class Animal:
+    def __init__(self, animal_name: str, make_sound: int = 1) -> None:
+        self.animal_name = animal_name # inicializuju si promenou v tride
+        self.make_sound = make_sound
+
+        if self.animal_name == "cat":
+            for i in range(self.make_sound):
+                print ("meow")
+        elif self.animal_name == "dog":
+            for i in range(self.make_sound):
+                print ("bark")
+
+    ###_________ na dalsi metodu teto classy koukni az pochopis to co je nahore
+    
+    def take_a_shit(self): # self pridava metode VSECHNY variables teto classy
+        print (f"{self.animal_name} is taking a shit")
+
+# ted ji jdu inicializovat
+# spust tento program
+
+dog_animal = Animal("dog", 1) # zde jde o psa a zasteka 1x
+cat_animal = Animal("cat", 3) # zde o kocku a zasteka 3x
+
+# - az pochopis to co je nad timto tak
+# odkomentuj to co je nize a spust znovu 
+# dog_animal.take_a_shit()
+# cat_animal.take_a_shit()
     
