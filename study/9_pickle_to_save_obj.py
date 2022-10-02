d4f418072c1f14b8f317f2a67f4d6a6197237478
petrsnake <petrsnake@gmail.com>
2022-10-02 20:21:44 +0200

    vyuka pickle

--- study/9_pickle_to_save_obj.py
+++ study/9_pickle_to_save_obj.py
@@ -5,3 +5,24 @@ exampleObj = {'python':3, 'kde':5, '10':5}
 fileObj = open('data.obj', 'wb')
 pickle.dump(exampleObj, fileObj)
 fileObj.close()
+
+#deserilaze object
+fileObj = open('data.obj', 'rb')
+exampleObj = pickle.load(fileObj)
+fileObj.close()
+print(exampleObj)
+
+
+
+
+#cvičení
+import pickle
+fileObj = open('file_for_fun', 'wb')
+file_for_fun = 'eskymáci snáší práci'
+pickle.dump(file_for_fun,fileObj)
+
+fileObj = open('file_for_fun', 'wb')
+deserialized = pickle.load(file_for_fun)
+file_for_fun.close()
+print(deserialized)
+
